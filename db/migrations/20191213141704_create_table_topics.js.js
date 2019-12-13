27a91bfca6d0dab05b4ef3fc99d16c7dfaaadb43
Afalls89 <andrewfalls@outlook.com>
2019-12-13 14:32:28 +0000

    created migration file for creating topics table. database tble created succsfully

---
+++ db/migrations/20191213141704_create_table_topics.js.js
@@ -0,0 +1,16 @@
+exports.up = function(knex) {
+	console.log("create topics table");
+	return knex.schema.createTable("topics", topicsTable => {
+		topicsTable
+			.string("slug")
+			.primary()
+			.unique()
+			.notNullable();
+		topicsTable.string("description");
+	});
+};
+
+exports.down = function(knex) {
+	console.log("drop topiocs table");
+	return knex.schema.dropTable("topics");
+};
