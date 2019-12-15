ecbd9cb94bbb103ccfefc1726f0cd48d881e33c0
Afalls89 <andrewfalls@outlook.com>
2019-12-15 16:22:31 +0000

    created and tested formatDates function to turn UNIX timestamps into timestamps thay PostGresSQL can accept

--- db/utils/utils.js
+++ db/utils/utils.js
@@ -1,4 +1,11 @@
-exports.formatDates = list => {};
+exports.formatDates = list => {
+	if (list.length === 0) return [];
+	return list.map(listObj => {
+		const newListObj = { ...listObj };
+		newListObj.created_at = new Date(newListObj.created_at);
+		return newListObj;
+	});
+};
 
 exports.makeRefObj = list => {};
 
--- spec/utils.spec.js
+++ spec/utils.spec.js
@@ -1,12 +1,90 @@
-const { expect } = require('chai');
+process.env.NODE_ENV = "test";
+
+const { expect } = require("chai");
 const {
-  formatDates,
-  makeRefObj,
-  formatComments,
-} = require('../db/utils/utils');
+	formatDates,
+	makeRefObj,
+	formatComments
+} = require("../db/utils/utils");
 
-describe('formatDates', () => {});
+describe("formatDates", () => {
+	it("if provided an empty array return a new empty array ", () => {
+		const data = [];
 
-describe('makeRefObj', () => {});
+		expect(formatDates(data)).to.deep.equal([]);
+		expect(formatDates(data)).to.not.equal(data);
+		expect(formatDates(data)).to.be.an("Array");
+	});
 
-describe('formatComments', () => {});
+	it("if given an array of objects, return new array of objects without manuipulating the originals", () => {
+		const data = [
+			{
+				title: "Living in the shadow of a great man",
+				topic: "mitch",
+				author: "butter_bridge",
+				body: "I find this existence challenging",
+				created_at: 1542284514171,
+				votes: 100
+			}
+		];
+
+		formatDates(data);
+
+		expect(data).to.deep.equal([
+			{
+				title: "Living in the shadow of a great man",
+				topic: "mitch",
+				author: "butter_bridge",
+				body: "I find this existence challenging",
+				created_at: 1542284514171,
+				votes: 100
+			}
+		]);
+	});
+
+	it("if provided an array with one object. the value for key 'created_at' should be changed from unix timestamp to standard timestamp format", () => {
+		const data = [
+			{
+				title: "Living in the shadow of a great man",
+				topic: "mitch",
+				author: "butter_bridge",
+				body: "I find this existence challenging",
+				created_at: 1542284514171,
+				votes: 100
+			}
+		];
+
+		expect(`${formatDates(data)[0].created_at}`).to.equal(
+			"Thu Nov 15 2018 12:21:54 GMT+0000 (Greenwich Mean Time)"
+		);
+	});
+
+	it("if provided an array with multiple objects. the value for key 'created_at' should be changed from unix timestamp to standard timestamp format", () => {
+		const data = [
+			{
+				title: "Living in the shadow of a great man",
+				topic: "mitch",
+				author: "butter_bridge",
+				body: "I find this existence challenging",
+				created_at: 1542284514171,
+				votes: 100
+			},
+			{
+				title: "Sony Vaio; or, The Laptop",
+				topic: "mitch",
+				author: "icellusedkars",
+				body:
+					"Call me Mitchell. Some years ago—never mind how long precisely—having little or no money in my purse, and nothing particular to interest me on shore, I thought I would buy a laptop about a little and see the codey part of the world. It is a way I have of driving off the spleen and regulating the circulation. Whenever I find myself growing grim about the mouth; whenever it is a damp, drizzly November in my soul; whenever I find myself involuntarily pausing before coffin warehouses, and bringing up the rear of every funeral I meet; and especially whenever my hypos get such an upper hand of me, that it requires a strong moral principle to prevent me from deliberately stepping into the street, and methodically knocking people’s hats off—then, I account it high time to get to coding as soon as I can. This is my substitute for pistol and ball. With a philosophical flourish Cato throws himself upon his sword; I quietly take to the laptop. There is nothing surprising in this. If they but knew it, almost all men in their degree, some time or other, cherish very nearly the same feelings towards the the Vaio with me.",
+				created_at: 1416140514171
+			}
+		];
+
+		expect(`${formatDates(data)[1].created_at}`).to.equal(
+			"Sun Nov 16 2014 12:21:54 GMT+0000 (Greenwich Mean Time)"
+		);
+	});
+});
+
+describe("makeRefObj", () => {});
+
+describe("formatComments", () => {});
